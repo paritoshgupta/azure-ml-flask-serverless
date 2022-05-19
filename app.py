2b9ca216f257e70353cbd36c7ccef152605a18b2
Paritosh Gupta <paritoshgupta93@gmail.com>
2022-05-19 19:17:38 +0530

    test

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Boston Housing Prediction Home: From Azure Pipelines/h3>"
+    html = "<h3>Boston Housing Prediction Home: From Azure Pipelines (CD)/h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
