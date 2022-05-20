a4220b629cefe000f56f7b5e5ced5cb237e72c4f
Paritosh Gupta <paritoshgupta93@gmail.com>
2022-05-20 17:53:11 +0530

    test

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Azure Pipelines Continuous Delivery (CD) Flask APP </h3>"
+    html = "<h3>Continuous Delivery (CD) Demo on Azure Pipelines using Flask APP </h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
